d33217019ba537e8c425612a3751488554825ed9
saadbenda <saad.benda@gmail.com>
2019-10-11 13:56:00 +0200

    add data base

--- data/script.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-print('Beginning file download with urllib2...')
-
-url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-urllib.request.urlretrieve(url, '/Users/scott/Downloads/cat.jpg')
